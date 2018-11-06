67cd2035e8a421b5f75d201bee617ef9d43d5ac8
best070 <bestiskim@gmail.com>
2018-11-07 00:05:38 +0900

    신규 파일 생성

--- daum_global_index.py
+++ daum_global_index.py
@@ -1,1 +1,86 @@
+import bs4
+from urllib.request import urlopen
+import pandas as pd
+import datetime as dt
+
 url = 'http://finance.daum.net/global/index_daily.daum?type=default&ric=/.GSPC&page=1'
+source = urlopen(url).read()
+source = bs4.BeautifulSoup(source, 'lxml')
+# print(source)
+
+dates = source.find_all('td', class_='datetime')
+print(dates)
+prices = source.find_all('td', class_='num')
+print("dates's length : %s" %len(dates))
+print("prices's length : ", len(prices))
+
+print('prices의 첫 번째 요인 : \n', prices[0].text)
+
+
+# 저자 github 코드 복사
+
+def date_format(d):
+    d = str(d).replace('-', '.')
+    yyyy = int(d.split('.')[0])
+    mm = int(d.split('.')[1])
+    dd = int(d.split('.')[2])
+    this_date = dt.date(yyyy, mm, dd)
+    return this_date
+
+historical_prices = dict()
+def historical_global_daum(index_cd, start_date='', end_date='', page_n=1, last_page=0):
+    if start_date:  # start_date가 있으면
+        start_date = date_format(start_date)  # date 포맷으로 변환
+    else:  # 없으면
+        start_date = dt.date.today()  # 오늘 날짜를 지정
+    if end_date:
+        end_date = date_format(end_date)
+    else:
+        end_date = dt.date.today()
+
+    url = 'http://finance.daum.net/global/index_daily.daum?type=default&ric=/.' + index_cd + '&page=' + str(page_n)
+
+    source = urlopen(url).read()
+    source = bs4.BeautifulSoup(source, 'lxml')
+
+    dates = source.find_all('td', class_='datetime')  # <td class="datetime">태그에서 날짜 수집
+    prices = source.find_all('td', class_='num')  # <td class="num">태그에서 날짜 수집
+
+    rows_in_page = len(dates)
+
+    if len(dates) > 0:
+
+        for n in range(rows_in_page):
+
+            # 날짜 처리
+            this_date = dates[n].text
+            this_date = date_format(this_date)
+
+            if this_date <= end_date and this_date >= start_date:
+                # start_date와 end_date 사이에서 데이터 저장
+                # 종가 처리
+                this_close = prices[n * 3].text
+                this_close = this_close.replace(' ', '')
+                this_close = this_close.replace('\t', '')
+                this_close = this_close.replace('\n', '')
+                this_close = this_close.replace(',', '')
+                this_close = float(this_close)
+
+                # 딕셔너리에 저장
+                historical_prices[this_date] = this_close
+
+            elif this_date < start_date:
+                # start_date 이전이면 함수 종료
+                return historical_prices
+                # 페이지 네비게이션
+        if rows_in_page == 10:
+            page_n = int(page_n)
+            page_n = page_n + 1
+
+            historical_global_daum(index_cd, start_date, end_date, page_n, last_page)
+
+    return historical_prices
+
+daum = historical_global_daum('GSPC', '2018-4-1', '2018-4-5')
+print("추출된 결과물 : \n", daum)
+
---
+++ reinforce_learning/reinforce_learning.py
@@ -0,0 +1,134 @@
+import mglearn
+import mglearn.datasets
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.datasets import load_breast_cancer
+import pprint
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+
+class reinforcement_learning():
+
+    def forge(self):
+        X, y = mglearn.datasets.make_forge()
+
+        mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+        plt.legend(['class0', "class1"], loc=4)
+        plt.xlabel("first_feature")
+        plt.ylabel("second_feature")
+        print("X.shape: {}".format(X.shape))
+        plt.show()
+
+    def make_wave(self):
+        X, y = mglearn.datasets.make_wave(n_samples=80)
+        plt.plot(X, y, 'o')
+        plt.ylim(-3, 3)
+        plt.xlabel("feature")
+        plt.ylabel("target")
+        plt.show()
+
+    def breast_cancer(self):
+        cancer=load_breast_cancer()
+        pprint.pprint(cancer)
+        print(type(cancer))
+        print("cancer.keys() : \n{}".format(cancer.keys()))
+        print("breast cancer data's shape:{}".format(cancer.data.shape))
+        print("num_samples by class:\n{}".format({n: v for n, v in zip(cancer.target_names, np.bincount(cancer.target))}))
+        print("feature_name:\n{}".format(cancer.feature_names))
+
+    def KNN_example(self):
+        mglearn.plots.plot_knn_classification(n_neighbors=1)
+        plt.show()
+        mglearn.plots.plot_knn_classification(n_neighbors=3)
+        plt.show()
+
+    def KNN_train_test(self):
+        X, y = mglearn.datasets.make_forge()
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+        clf = KNeighborsClassifier(n_neighbors=3)
+        clf.fit(X_train, y_train)
+        print("test set prediction: {}".format(clf.predict(X_test)))
+        print("test set accuracy: {:.2f}".format(clf.score(X_test, y_test)))
+
+    def KNeighborsclassifier(self):
+        X, y = mglearn.datasets.make_forge()
+        fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+        for n_neighbors, ax in zip([1, 3, 9], axes):
+            # fit method는 self object를 return 함
+            # 그래서 object create와 fit method를 한 줄에 쓸 수 있다.
+            clf = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+            mglearn.plots.plot_2d_separator(clf, X, fill=True, eps=0.5, ax=ax, alpha=0.4)
+            mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+            ax.set_title("{} neighbor".format(n_neighbors))
+            ax.set_xlabel("feature0")
+            ax.set_ylabel("feature1")
+        axes[0].legend(loc=3)
+        plt.show()
+
+    def breast_cancer_train_test(self):
+        cancer = load_breast_cancer()
+        X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+        training_accuracy = []
+        test_accuracy = []
+        # 1에서 10까지 n_neighbors를 적용
+        neighbors_settings = range(1, 11)
+        for n_neighbors in neighbors_settings:
+            # create model
+            clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+            clf.fit(X_train, y_train)
+            # trainset accuracy save
+            training_accuracy.append(clf.score(X_train, y_train))
+            # testset accuracy save
+            test_accuracy.append(clf.score(X_test, y_test))
+        plt.plot(neighbors_settings, training_accuracy, label="training_accuracy")
+        plt.plot(neighbors_settings, test_accuracy, label="test_accuracy")
+        plt.ylabel("accuracy")
+        plt.xlabel("n_neighbors")
+        plt.legend()
+        # plt.show()
+        # k_neighbors_regression
+        mglearn.plots.plot_knn_regression(n_neighbors=1)
+        mglearn.plots.plot_knn_regression(n_neighbors=3)
+        plt.show()
+
+    def k_neighbors_regression(self):
+        X, y = mglearn.datasets.make_wave(n_samples=40)
+
+        # wave dataset을 train set과 test set으로 나눔
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+        # neighbors num을 3으로 해서 model의 object를 만듦
+        reg = KNeighborsRegressor(n_neighbors=3)
+        reg.fit(X_train, y_train)
+        print('testset predict:\n{}'.format(reg.predict(X_test)))
+        print('testset R^2: {:.2f}'.format(reg.score(X_test, y_test)))
+        fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+        # -3과 3 사이에 1000개의 data point를 만든다.
+        line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+        for n_neighbors, ax in zip([1, 3, 9], axes):
+            # 각 1, 3, 9개의 neighbors를 사용해 예측을 함
+            reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+            reg.fit(X_train, y_train)
+            ax.plot(line, reg.predict(line))
+            ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+            ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+
+            ax.set_title(
+                "{} neighbor's train score: {:.2f} test score: {:.2f}".format(
+                    n_neighbors, reg.score(X_train, y_train), reg.score(X_test, y_test)))
+            ax.set_xlabel('feature')
+            ax.set_ylabel('target')
+        axes[0].legend(['model predict', 'train data/target', 'test data/target'], loc="best")
+        plt.show()
+
+
+
+
+# reinforcement_learning().breast_cancer()
+# reinforcement_learning().KNN_example()
+# reinforcement_learning().KNN_train_test()
+# reinforcement_learning().KNeighborsclassifier()
+# reinforcement_learning().breast_cancer_train_test()
+reinforcement_learning().k_neighbors_regression()
