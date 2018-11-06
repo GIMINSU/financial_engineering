61cee6f747bdb87795ede17ffd152b5241c87219
best070 <bestiskim@gmail.com>
2018-11-07 01:15:16 +0900

    logisticregression

--- reinforce_learning/linear_model.py
+++ reinforce_learning/linear_model.py
@@ -11,6 +11,8 @@ from sklearn.neighbors import KNeighborsRegressor
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 
 class LinearModel():
     def linear_regression(self):
@@ -63,4 +65,22 @@ class LinearModel():
         mglearn.plots.plot_ridge_n_samples()
         plt.show()
 
-LinearModel().linear_regression()
+    def logisticregression(self):
+
+        X, y = mglearn.datasets.make_forge()
+
+        fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+        for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+            clf = model.fit(X, y)
+            mglearn.plots.plot_2d_separator(clf, X, fill=False, eps=0.5, ax=ax, alpha=0.7)
+            mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+            ax.set_title("{}".format(clf.__class__.__name__))
+            ax.set_xlabel("feature 0 ")
+            ax.set_ylabel("feature 1")
+        axes[0].legend()
+
+        mglearn.plots.plot_linear_svc_regularization()
+        plt.show()
+# LinearModel().linear_regression()
+LinearModel().logisticregression()
